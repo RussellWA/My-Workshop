b9606ff1270cf6c1404309a1ea050a18b8f38e80
Russell <rvssell.wa@gmail.com>
2026-07-13 15:32:27 +0700

    feat: head space for detail hero

--- components/invention/DetailHero.tsx
+++ components/invention/DetailHero.tsx
@@ -20,7 +20,7 @@ export default function DetailHero({name, type, slug, attunement, quote, image, 
     const theme = SIGNATURE_THEME[signature]
 
     return (
-        <section className="relative flex flex-col items-center justify-center w-full px-10">
+        <section className="relative flex flex-col items-center justify-center w-full px-10 pt-10">
             <div className="relative mb-8 md:mb-16 flex flex-col items-center justify-center">
                 <motion.div 
                     className={`absolute top-1/2 left-1/2 w-48 h-48 rounded-full blur-3xl -z-10 origin-center pointer-events-none ${theme.heroGlow}`}
